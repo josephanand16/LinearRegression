0bf3a959507abcf02631c14c769356e1597076ee
josephanand16 <31557535+josephanand16@users.noreply.github.com>
2018-07-08 21:22:52 +0530

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,12 +4,12 @@ import math
 import statistics
 import matplotlib.pyplot as plt
 
-land_price = [50,69,80,30,15,150,250]
-sqft100 = [19,22,28,12,6,30,50]
+land_price = [50,69,80,30,15,150,250,300,500]
+sqft100 = [19,22,28,12,6,30,50,56,60]
 Xmean = numpy.mean(land_price)
 Ymean = numpy.mean(sqft100)
 
-n = len(X)
+n = len(land_price)
 
 varX = statistics.variance(land_price-Xmean)
 covXY = numpy.cov(land_price,sqft100)[0][1]
@@ -25,9 +25,9 @@ def graph(formula, x_range):
     plt.plot(x, y)  
     plt.show()
 
-plt.plot(X, Y, 'ro')
+plt.plot(land_price, sqft100, 'ro')
 plt.xlabel("100Square_ft")
 plt.ylabel("land_price in lakhs")
-plt.axis([0, 300, 0, 150])
-graph("%f*x + %f" % (slope,intercept), range(0, 300))
+plt.axis([0, 600, 0, 150])
+graph("%f*x + %f" % (slope,intercept), range(0, 600))
 plt.show()
